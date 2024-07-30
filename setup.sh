c50f54b6b2f6b86a25e1971a9b10dddd98007eba
ahenven <ahendersonvenables@gmail.com>
2024-07-30 17:22:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,13 @@ cd xmrig/build
 echo "Running cmake and make..."
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
 
+# Move config.json to the build directory
 echo "Moving config.json..."
 mv ../../scripts/config.json .
+
+# Move the contents of the build directory to the directory behind scripts
+echo "Moving build contents to the parent directory behind scripts..."
+cd ..
+mv build/* ../scripts/xmrig/
 
 echo "Setup complete."
